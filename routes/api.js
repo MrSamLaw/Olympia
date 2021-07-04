26b7d1259ba0026996f7922c869303a9151bd6a6
Mr Sam Law <sam@chongland.com>
2021-07-04 17:29:39 +1000

    HTML Routes & workouts range route

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,51 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+
+// Get all workouts, with total duration of exercises
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.length" },
+            },
+        },
+    ])
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.length" },
+            },
+        },
+    ])
+        // Sort & limit data
+        .sort({ _id: -1 })
+        .limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// Create workout
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
